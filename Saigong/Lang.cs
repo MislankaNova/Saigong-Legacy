1f656eb82d46e3f7e3874acd9b39f4f649fa8748
MislankaNova <mislankanova@hotmail.com>
2015-11-30 22:42:59 +0800

    The missing Lang.cs is now included in the primary branch. Sorry.

---
+++ Saigong/Lang.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saigong
+{
+    /*
+     * TODO: Load language settings from a language file in the future!
+     */
+    static class Lang
+    {
+        /*
+         * 紀念在世不到一年的関越文字
+         * 
+         * public static string title = "関越文字";
+         * 
+         */
+
+        public static string title = "西江";
+
+        public static string startupFinished = "万事俱备";
+
+        public static string saved = "已存储";
+        public static string loaded = "已载入";
+        public static string planLoaded = "大纲已载入";
+        public static string loadFail = "载入失败";
+        public static string planLoadFail = "未能载入大纲";
+        public static string nullTitle = "标题为空";
+        public static string nullOperation = "内容为空";
+        public static string notFound = "未有找到";
+        public static string alreadyFound = "已是最后一处";
+        public static string found = "已找到";
+        public static string backupDone = "已备份";
+
+        public static string chara = "字";
+
+        public static string shutdown = "再见";
+        public static string batsu = "X";
+    }
+}
