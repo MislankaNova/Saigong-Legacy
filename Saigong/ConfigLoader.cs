6bae7ad9dd1074b7194ff84118a31c9977c7ad44
MislankaNova <mislankanova@hotmail.com>
2015-12-05 13:48:44 +0800

    Used 'var' instead of specific type name in ConfigLoader

---
+++ Saigong/ConfigLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.IO;
+
+namespace Saigong
+{
+    static class ConfigLoader
+    {
+        public static Dictionary<string, string> LoadConfigFile(string dir)
+        {
+            if (! File.Exists(dir))
+            {
+                return null;
+            }
+
+            var result = new Dictionary<string, string>();
+            var sr = File.OpenText(dir);
+
+            while (! sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] splited = line.Split(':');
+                if (splited.Length == 2)
+                {
+                    result[splited[0]] = splited[1];
+                }
+            }
+            sr.Close();
+
+            return result;
+        }
+    }
+}
